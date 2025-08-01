53edf0a1ce96d3d53d5cc81e81f5b4a6795868da
apast0r <9809148+TheNextLVL@users.noreply.github.com>
2025-08-01 21:44:29 +0200

    Update deprecated IR SCHEMA

--- components/climate_ir_panasonic/climate.py
+++ components/climate_ir_panasonic/climate.py
@@ -6,7 +6,7 @@ AUTO_LOAD = ["climate_ir"]
 climate_ir_panasonic_ns = cg.esphome_ns.namespace("climate_ir_panasonic")
 PanasonicClimate = climate_ir_panasonic_ns.class_("PanasonicClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HitachiClimate)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(PanasonicClimate)
 
 async def to_code(config):
     await climate_ir.new_climate_ir(config)
